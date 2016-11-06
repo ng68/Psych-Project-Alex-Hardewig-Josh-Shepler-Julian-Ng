9389ccbb078a5f1176d4444b061400abfc2fe2d1
ng68 <ng68@purdue.edu>
2016-11-06 14:44:46 -0500

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,1 +1,13 @@
 
+import java.awt.*;
+
+/**
+ * Created by julianc.ng on 10/30/16.
+ */
+public class Model {
+    Controller controller;
+    public Model(Controller controller) {
+        this.controller = controller;
+
+    }
+}
